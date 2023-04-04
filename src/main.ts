96f46a64ab4bceecdb1b043feb77d46787f62e34
Dai Nguyen <ngtrdai290197@gmail.com>
2023-04-05 03:36:36 +0700

    Add exit code and prefix api

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EnvironmentService } from './environment/environment.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+  const envService = app.get(EnvironmentService);
+
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  app.setGlobalPrefix('/api');
+
+  const PORT = envService.get<number>('api_port');
+  await app.listen(PORT, () => {
+    Logger.log('API listening on port: ', PORT);
+  });
 }
 bootstrap();
